864a7722f2ba06d63e19b9be96a526aff1c105e1
Fredohm <fredwaucquez@hotmail.fr>
2022-02-21 07:50:40 +0100

    Set background image of the US

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# US States game
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+
+screen.exitonclick()
