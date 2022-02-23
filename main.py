322421f3bfe0074dd7ef9f637b92e1bbc74b92a7
Fredohm <fredwaucquez@hotmail.fr>
2022-02-23 16:01:26 +0100

    add creation of csv file for missing states. RELEASE 1.0

--- main.py
+++ main.py
@@ -19,17 +19,26 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-guessed_sates = []
+guessed_states = []
 
-while len(guessed_sates) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_sates)}/50 States Correct",
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
+        missing_states = []
+        for state in df.state:
+            if state not in guessed_states:
+                missing_states.append(state)
+                missing_states_dict = {
+                    "state": missing_states
+                }
+                states_to_learn = pandas.DataFrame(missing_states_dict)
+                states_to_learn.to_csv("states_to_learn.csv")
         break
 
     for state in df.state:
         if state == answer_state:
-            if state not in guessed_sates:
-                guessed_sates.append(state)
+            if state not in guessed_states:
+                guessed_states.append(state)
                 state_row = df[df.state == state]
                 write_state(state_row.state.item(), int(state_row.x), int(state_row.y))
